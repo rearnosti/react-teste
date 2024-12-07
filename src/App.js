2db834ad3133582a033a17e610b7228668fd3c41
rearnosti <renan.arnosti@gmail.com>
2024-12-07 16:55:42 -0300

    .
    .

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from "./pages/Login"
+import Grid from '@mui/material/Grid';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
